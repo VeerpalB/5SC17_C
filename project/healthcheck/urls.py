4dc61faceb6e93aaa320b4d9eaf6088811539bd6
VeerpalB <w2045579@westminster.ac.uk>
2025-04-28 17:48:00 +0100

    Updates to pages

--- project/healthcheck/urls.py
+++ project/healthcheck/urls.py
@@ -6,13 +6,13 @@ from . import views
 
 urlpatterns = [
     path('user', views.home, name='home_name'),
-    path('profile/', views.profile, name='profile_name'),
-    path('progress/', views.progress, name='progress_name'),
-    path('help/', views.help, name='help_name'),
-    path('logout/', views.logout, name='logout_name'),
-    path('voting/', views.voting, name='voting_name'),
-    path('', views.login, name='login_name'),
-    path('signup/', views.signup, name='signup_name'),
+    path('profile', views.profile, name='profile_name'),
+    path('progress', views.progress, name='progress_name'),
+    path('help', views.help, name='help_name'),
+    path('logout', views.logout, name='logout_name'),
+    path('voting', views.voting, name='voting_name'),
+    path('login', views.login, name='login_name'),
+    path('signup', views.signup, name='signup_name'),
 
 
     path('navbar/', views.navbar),
