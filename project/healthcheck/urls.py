489498ebfec2bfdc8aaea37e187774c4f1141d05
VeerpalB <w2045579@westminster.ac.uk>
2025-04-29 19:20:01 +0100

    made changes to urls.py

--- project/healthcheck/urls.py
+++ project/healthcheck/urls.py
@@ -6,11 +6,11 @@ from . import views
 
 urlpatterns = [
     path('user', views.home, name='home_name'),
-    path('profile', views.profile, name='profile_name'),
-    path('progress', views.progress, name='progress_name'),
-    path('help', views.help, name='help_name'),
-    path('logout', views.logout, name='logout_name'),
-    path('voting', views.voting, name='voting_name'),
+    path('profile/', views.profile, name='profile_name'),
+    path('progress/', views.progress, name='progress_name'),
+    path('help/', views.help, name='help_name'),
+    path('logout/', views.logout, name='logout_name'),
+    path('voting/', views.voting, name='voting_name'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('forgotten_password/', views.forgotten_password, name='forgotten_password'),
