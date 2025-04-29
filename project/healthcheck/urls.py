157138e10227b5241cedcb2811fc1fb753d58dc1
anoor71 <azhaarno@gmail.com>
2025-04-29 19:23:19 +0100

    Added in background

--- project/healthcheck/urls.py
+++ project/healthcheck/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     path('voting/', views.voting, name='voting_name'),
     path('session/', views.session, name='session_name'),
     path('dashboard/', views.dashboard, name='dashboard_name'),
-
     path('profile', views.profile, name='profile_name'),
     path('progress', views.progress, name='progress_name'),
     path('help', views.help, name='help_name'),
